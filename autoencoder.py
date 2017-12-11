77dab373c149c27d941cba30e5c8fa11942b5a50
gardenia22 <nanshu.wang@gmail.com>
2017-12-10 16:47:20 -0800

    update autoencoder

--- autoencoder.py
+++ autoencoder.py
@@ -1,10 +1,8 @@
 from __future__ import division, print_function, absolute_import
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 import os
-import mfcc
 
 mode = "train" # pred or train
 epoch_start = 1
@@ -76,15 +74,15 @@ biases = {
 
 # Building the encoder
 def encoder(x):
-    layer_1 = tf.nn.tanh(tf.add(tf.matmul(x, weights['encoder_h1']),biases['encoder_b1']))
-    layer_2 = tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['encoder_h2']),biases['encoder_b2']))
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),biases['encoder_b1']))
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),biases['encoder_b2']))
     return layer_2
 
 
 # Building the decoder
 def decoder(x):
-    layer_1 = tf.nn.tanh(tf.add(tf.matmul(x, weights['decoder_h1']),biases['decoder_b1']))
-    layer_2 = tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['decoder_h2']),biases['decoder_b2']))
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),biases['decoder_b1']))
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),biases['decoder_b2']))
     return layer_2
 
 
